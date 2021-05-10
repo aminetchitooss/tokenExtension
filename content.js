c5d72a6a65cde76af3e89d6b3ebe0f611f4b4a1a
Amine tchita <amine.tchita1@gmail.com>
2021-05-10 14:48:14 +0200

    async refacto

--- content.js
+++ content.js
@@ -1,8 +1,30 @@
 
 // console.log("testing ...")
+let COUNT = 0
+const RETRY_LIMIT = 3
+chrome.runtime.onMessage.addListener(gotMessage)
 
-chrome.runtime.onMessage.addListener(getToken)
-
-function getToken(data, sender, sendResponse) {
-    sendResponse({ token: localStorage['accessToken']?.trim() })
+function getToken() {
+    return new Promise(resolve => {
+        if (localStorage['accessToken']?.trim())
+            resolve(localStorage['accessToken'].trim())
+        else if (COUNT < RETRY_LIMIT) {
+            COUNT++;
+            console.log('Retrying getting token ...')
+            setTimeout(() => {
+                resolve(getToken())
+            }, 1000);
+        }
+        else
+            resolve(null)
+    })
+}
+function gotMessage(data, sender, sendResponse) {
+    (async () => {
+        COUNT = 0
+        const payload = await getToken();
+        console.log('got it')
+        sendResponse({ token: payload })
+    })();
+    return true;
 }
